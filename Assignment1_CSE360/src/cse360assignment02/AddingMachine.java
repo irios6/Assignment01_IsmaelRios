7734063a615b52f7b97e72571ae6d5b853b37dd7
ismae <ismae@DESKTOP-R9439PH>
2020-10-05 23:17:54 -0700

    Second version of AddingMachine.

--- Assignment1_CSE360/src/cse360assignment02/AddingMachine.java
+++ Assignment1_CSE360/src/cse360assignment02/AddingMachine.java
@@ -2,6 +2,7 @@ package cse360assignment02;
 
 public class AddingMachine {
 	private int total; //declare total
+	private String history = "0"; //declare and initialize history to a string containing 0
   
 	//constructor
 	public AddingMachine () {
@@ -9,19 +10,36 @@ public class AddingMachine {
 	}
   
 	public int getTotal () {
-		return 0;
+		return total; //return the current total
 	}
   
 	public void add (int value) {
+		total = total + value; //update total
+		history += " + " + value; //update history
 	}
 
 	public void subtract (int value) {
+		total = total - value; //update total
+		history += " - " + value; //update history
 	}
 
 	public String toString () {
-		return ""; //return empty string
+		return history; //return string containing the history of the add/subtract calculations
 	}
 
 	public void clear() {
+		history = null; //clear "memory"
 	}
-}//end of class
+	
+	//test functions
+	public static void main(String[] args) {
+		AddingMachine machine = new AddingMachine(); //create AddingMachine object
+		
+		machine.add(4); //call add to add 4 
+		machine.subtract(2); //call subtract to subtract 2
+		machine.add(5); //call add to add 5
+		
+		System.out.println(machine.toString()); //call toString() to print out history
+		machine.clear(); //call clear() to clear "memory"
+	} 
+}//end of AddingMachine class
